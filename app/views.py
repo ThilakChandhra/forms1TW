3b764b21a2d64b9232d62d7f3b438b8adbdfdd6b
ThilakChandhra <thilaknagisetty17@gmail.com>
2023-05-25 15:46:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from app.models import *
+def topic(request):
+    if request.method=='POST':
+        tn=request.POST['tn']
+        TO=Topic.objects.get_or_create(topic_name=tn)[0]
+        TO.save()
+        return HttpResponse('Topic insertion is done Successfully')
+
+    return render(request,'topic.html')
+
+def webpage(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+
+    if request.method=='POST':
+        topic=request.POST['topic']
+        name=request.POST.get('name')
+        url=request.POST.get('url')
+        email=request.POST['email']
+        TO=Topic.objects.get(topic_name=topic)
+
+        WO=Webpage.objects.get_or_create(topic_name=TO,name=name,url=url,email=email)[0]
+        WO.save()
+        return HttpResponse('Webpage insertion is done Successfully')
+
+
+    return render(request,'webpage.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
